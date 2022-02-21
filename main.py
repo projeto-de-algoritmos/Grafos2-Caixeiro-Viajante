6b124272ad1ef2e41fbeaaea38fbf5a4b560a316
ServidioEC <52542729+ServidioEC@users.noreply.github.com>
2022-02-21 01:09:30 -0300

    Implementação do CSS e Upgrade do jogo

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import random
 import logging
 import sys
 
-vetorPontosTuristicos = ["Praia Leblon", "Rocinha", "Lagoa", "Jardim Botanico", " Cristo Redentos",
-                         "Sambodramo", "Aeroporto", "Praia de Copacana", "Pão De Acucar", "Bahia de Guanabara"
+vetorPontosTuristicos = ["Praia Leblon", "Rocinha", "Lagoa", "Jardim Botânico", " Cristo Redentor",
+                         "Sambódramo", "Aeroporto", "Praia de Copacabana", "Pão De Açúcar", "Baía de Guanabara"
                          ]
-
 
 class Graph():
 
@@ -16,20 +15,15 @@ class Graph():
         self.graph = [[0 for column in range(vertices)]
                       for row in range(vertices)]
 
-    # A utility function to print the constructed MST stored in parent[]
     def printMST(self, parent):
         resultMap = ""
         for i in range(1, self.V):
-            resultMap += "{} -  {} \t {}\n".format(
+            resultMap += '{} -  {} \t R$ {},00.'.format(
                 vetorPontosTuristicos[parent[i]], vetorPontosTuristicos[i], self.graph[i][parent[i]])
-        return resultMap
+        return resultMap 
 
-    # A utility function to find the vertex with
-    # minimum distance value, from the set of vertices
-    # not yet included in shortest path tree
     def minKey(self, key, mstSet):
 
-        # Initialize min value
         min = sys.maxsize
 
         for v in range(self.V):
@@ -39,55 +33,26 @@ class Graph():
 
         return min_index
 
-    # Function to construct and print MST for a graph
-    # represented using adjacency matrix representation
     def primMST(self):
 
-        # Key values used to pick minimum weight edge in cut
         key = [sys.maxsize] * self.V
-        parent = [None] * self.V  # Array to store constructed MST
-        # Make key 0 so that this vertex is picked as first vertex
+        parent = [None] * self.V  
         key[0] = 0
         mstSet = [False] * self.V
 
-        parent[0] = -1  # First node is always the root of
+        parent[0] = -1  
 
         for cout in range(self.V):
-
-            # Pick the minimum distance vertex from
-            # the set of vertices not yet processed.
-            # u is always equal to src in first iteration
             u = self.minKey(key, mstSet)
-
-            # Put the minimum distance vertex in
-            # the shortest path tree
             mstSet[u] = True
-
-            # Update dist value of the adjacent vertices
-            # of the picked vertex only if the current
-            # distance is greater than new distance and
-            # the vertex in not in the shortest path tree
             for v in range(self.V):
-
-                # graph[u][v] is non zero only for adjacent vertices of m
-                # mstSet[v] is false for vertices not yet included in MST
-                # Update the key only if graph[u][v] is smaller than key[v]
                 if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]:
                     key[v] = self.graph[u][v]
                     parent[v] = u
-
         return(self.printMST(parent))
 
 
-app = Flask(__name__)  # sempre ao iniciar um site
-
-
-# criar a primeira página do site
-# route -> caminho que vem depois do dominio
-
-# funcao -> o que quero exibir naquela página
-# template
-
+app = Flask(__name__) 
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
@@ -143,7 +108,6 @@ def homepage():
                            aresta18=aresta18,
                            aresta19=aresta19,
                            resultado=resultado)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
